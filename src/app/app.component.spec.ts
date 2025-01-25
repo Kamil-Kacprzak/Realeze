b246e32d2dd3e47a4ec0d9b34d86daa8e96ca48a
Kamil-Kacprzak <Kamil.Kacprzak@yahoo.com>
2025-01-25 16:29:55 +0100

    Updated project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'daily-planner' title`, () => {
+  it(`should have the 'realeze' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('daily-planner');
+    expect(app.title).toEqual('realeze');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, daily-planner');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, realeze');
   });
 });
